6ebceb5d73d2411ee6d5e2e23c68c8e79f442d56
Oscar Bautista <oscar04262000@gmail.com>
2023-11-09 17:41:56 -0600

    Refractor: separa las secciones

--- src/components/Presentacion.js
+++ src/components/Presentacion.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HeaderNav } from './layout/HeaderNav';
 
 export const Presentacion = () => {
     return (
-        <div className="presentacion">
-            <p className="bienvenida"> ¡Saludos! </p>
-            <h2> Desarrollador FrontEnd y BackEnd</h2>
-            <h3>  Oscar Bautista Gaytan  </h3>
-            <p className="descripcion"> Programador FrontEnd y BackEnd, desarrollo sitios web
-            dinamicos, con diseños atractivos, para la administración y el manejo de datos de tu negocio </p>
-            
-            <Link to="/portafolio" > Visitar portafolio </Link>
-        </div>
+        <section id="inicio">
+            <div className="contenido">
+
+                {/* Barra de nevegación */}
+                <HeaderNav></HeaderNav>
+                
+                <div className="presentacion">
+                    <p className="bienvenida"> ¡Saludos! </p>
+                    <h2> Desarrollador FrontEnd y BackEnd</h2>
+                    <h3>  Oscar Bautista Gaytan  </h3>
+                    <p className="descripcion"> Programador FrontEnd y BackEnd, desarrollo sitios web
+                    dinamicos, con diseños atractivos, para la administración y el manejo de datos de tu negocio </p>
+                    
+                    <Link to="/portafolio" > Visitar portafolio </Link>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -8,22 +8,20 @@ import { SobreMi } from '../components/SobreMi';
 export const MisRutas = () => {
     return (
         <BrowserRouter>
-            <section id="inicio">
+            <Routes>
+                <Route path="/" element={ <Navigate to="/presentacion"></Navigate>} ></Route>
+                <Route path="/presentacion" element={<Presentacion></Presentacion>}></Route>
+                <Route path="/portafolio" element={<Portafolio></Portafolio>}></Route>
+                <Route path="/sobremi" element={<SobreMi></SobreMi>}></Route>
+            </Routes>
+            {/* <section id="inicio">
                 <div className="contenido">
 
-                    {/** Header */}
-                    <HeaderNav></HeaderNav>
-
-                    <Routes>
-                        <Route path="/" element={ <Navigate to="/presentacion"></Navigate>} ></Route>
-                        <Route path="/presentacion" element={<Presentacion></Presentacion>}></Route>
-                        <Route path="/portafolio" element={<Portafolio></Portafolio>}></Route>
-                        <Route path="/sobremi" element={<SobreMi></SobreMi>}></Route>
-                    </Routes>
+                    
 
                     
                 </div>
-            </section>
+            </section> */}
         </BrowserRouter>
     )
 }
